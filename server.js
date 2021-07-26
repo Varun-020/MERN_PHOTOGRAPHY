e928ab350dc5d86445b0c44375a909fa27fc7ec9
Varun Mishra <76841264+Varun-020@users.noreply.github.com>
2021-07-26 21:07:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,12 @@ const path = require('path');
 require("dotenv").config();
 
 const app = express();
-
-
 const port = process.env.PORT;
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'/client/build/')));
-    app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname,'/client/build/')));
+app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
-}
 
 
 //database connection
